2a92b727eac5cb636559483df94487f77b012742
Fran Iglesias <fran.iglesias@qualifyze.com>
2024-11-26 19:36:07 +0100

    Introduce minimal support for configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ProposalModule } from './proposals/ProposalModule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CqrsModule, ProposalModule],
+  imports: [
+    CqrsModule,
+    ProposalModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
